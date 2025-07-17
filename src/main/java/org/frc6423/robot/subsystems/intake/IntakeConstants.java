9a0bd4e381f85a879955652c1fc07d25fb72e5a5
Avenger623 <noahethandurst@gmail.com>
2025-07-17 01:11:09 +0000

    asdfhasdlkfj
    Signed-off-by: Avenger623 <noahethandurst@gmail.com>

---
+++ src/main/java/org/frc6423/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,12 @@
+package org.frc6423.robot.subsystems.intake;
+
+public class IntakeConstants {
+    public static final int PIVOT_CAN_ID = 12;
+    public static final String PIVOT_CAN_BUS = "main";
+
+    public static final int PIVOT_ABS_ENCODER_CAN_ID = 13;
+    public static final String PIVOT_ABS_ENCODER_CAN_BUS = "main";
+
+    public static final int ROLLER_CAN_ID = 13;
+    public static final String ROLLER_CAN_BUS = "main";
+}
